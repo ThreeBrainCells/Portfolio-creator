6d7b38dba384610554e3f50c938a96d5e01fc1a9
Graeme Montrose <teentrose@gmail.com>
2022-02-23 20:33:50 -0700

    added fs skeleton

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+var htmlContent = `<html><br>
+<head><br>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head><br>
+</html>`;
+
+fs.writeFile('/my-portfolio.html', htmlContent, (err) => 
+err ? console.error(err) : console.log('Success!'));

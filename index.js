38dc2469bae3916c77280736cc800fc67843f2f1
Graeme Montrose <teentrose@gmail.com>
2022-02-23 21:03:25 -0700

    minor formatting

--- index.js
+++ index.js
@@ -30,14 +30,14 @@ inquirer.prompt([
     },
   ]).then(data => console.table(data));
 
-let htmlContent = `<html><br>
-<head><br>
+htmlContent = `<html>
+<head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>${data.name}'s Portfolio</title>
-</head><br>
-<body><br>
+</head>
+<body>
 </body>
 </html>`;
 
